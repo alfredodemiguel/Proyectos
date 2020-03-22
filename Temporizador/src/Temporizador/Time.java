7cfa9376068f573a7bb9f0055a2191a5de9041e0
alfredodemiguel <alfredodemiguel@yahoo.es>
2020-03-22 11:43:44 +0100

    Linux

---
+++ Temporizador/src/Temporizador/Time.java
@@ -0,0 +1,10 @@
+
+package Temporizador;
+
+
+public class Time {
+    public static void main(String[] args) {
+        Temporizador t1 = new Temporizador ();
+        t1.setVisible(true);
+    }
+}
