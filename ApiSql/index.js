666b2b410feb3487abb405e8aaed1e317c774d38
alfredodemiguel <alfredodemiguel@yahoo.es>
2021-07-09 09:42:43 +0200

    get funcionando

--- ApiSql/index.js
+++ ApiSql/index.js
@@ -1,22 +1,35 @@
 const rest = new (require('rest-mssql-nodejs'))({
     user:'sa',
     password:'Sovasa00',
-    server: '192.168.17.216',
+    server: '192.168.17.91',
     port: 1433,
     database: 'Therefore'
     });
 const express = require('express');
 const morgan = require ('morgan');
+const fs = require('fs');
+const https = require('https');
 const bodyParser = require('body-parser');
-var cors = require('cors-express');
+const cors = require('cors-express');
 const app = express();
-const port = process.env.PORT || 3017;
+
+const PUERTO = 3030;
+
+https.createServer({
+   cert: fs.readFileSync('alfredodemiguel.crt'),
+   key: fs.readFileSync('alfredodemiguel.key')
+ },app).listen(PUERTO, function(){
+	console.log('Servidor https correindo en el puerto 3030');
+});
+
+
 
 
 app.use (morgan('dev'));
 app.use(express.static('.'));
 app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+
 options = {
     allow : {
         origin: '*',
@@ -27,23 +40,31 @@ options = {
  
 app.use(cors(options));
 
-var providers = [];
-
 
 app.get ('/', (req, res) => res.send ('Incorrect URL.'));
 
 
 
 app.get('/provider/:id', function(req, res) {
-    let selectedProvider;
+    let selectedProvider,PNombre,PDireccion,PTelefono,PFax;;
+    var { id } = req.params; 
+    id = id.split(':');
+    id = id[1];
 
-    const { id } = req.params;      
-    let PNombre = "ACME";
-    let PDireccion = "Rue del Percebe sn";
-    let PTelefono = "123456";
-    let PFax = "0987";
-    selectedProvider = {id:id,PNombre:PNombre,PDireccion:PDireccion,PTelefono:PTelefono,PFax:PFax};
-    res.send(selectedProvider);
+    setTimeout (async() =>{
+        let resultado = await rest.executeQuery ('select * from proveedores where id = @id',[{
+            name: 'id',
+            type:'varchar',
+            value: id
+        }]);
+        resultado = resultado.data[0][0];
+        PNombre = resultado.nombre;
+        PDireccion = resultado.direccion;
+        PTelefono = resultado.telefono;
+        PFax = resultado.fax;
+        selectedProvider = {id:id,PNombre:PNombre,PDireccion:PDireccion,PTelefono:PTelefono,PFax:PFax};
+        res.send(selectedProvider);
+        },1500);
 });
 
 
@@ -59,6 +80,7 @@ app.post('/provider', (req, res) => {
     let PDireccion = req.body.PDireccion;
     let PTelefono = req.body.PTelefono;
     let PFax = req.body.PFax;
+
     
       
     setTimeout (async() =>{
@@ -71,12 +93,10 @@ app.post('/provider', (req, res) => {
         },1500);
 
 
-        
+
     let selectedProvider = {id:id,PNombre:PNombre,PDireccion:PDireccion,DTelefono:PTelefono,PFax:PFax};
     res.send(selectedProvider);
 });
 
-app.listen(port, () => {
- console.log("El servidor está inicializado en el puerto: " + port);
-});
+
  
