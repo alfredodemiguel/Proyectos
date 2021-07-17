b558fe65ea2d75ee38bf96fe072d81f52de9b87f
alfredodemiguel <alfredodemiguel@yahoo.es>
2021-07-17 17:29:28 +0200

    antes try catch

---
+++ ApiSql/Basic/Temporizador.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo "Lanzamiento de Temporizador"
+cd /home/alfredo/Documentos/Varios/Cosas
+pwd
+java -jar "Temporizador.jar"
+
--- ApiSql/Basic/index.js
+++ ApiSql/Basic/index.js
@@ -1,4 +1,8 @@
+require ('dotenv').config();
+const exec = require('child_process').exec
+//const Shell = require('shelljs')
 const mwBasicAuth = require ('./mwBasicAuth');
+/*
 const rest = new (require('rest-mssql-nodejs'))({
     user:'sa',
     password:'Sovasa00',
@@ -6,6 +10,7 @@ const rest = new (require('rest-mssql-nodejs'))({
     port: 1433,
     database: 'Therefore'
     });
+*/
 const express = require('express');
 const morgan = require ('morgan');
 const bodyParser = require('body-parser');
@@ -18,15 +23,12 @@ app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 app.use (mwBasicAuth);
 
-const port = process.env.PORT || 3030
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log("The server is starting on port " + port);
 });
     
-
-app.get ('/', (req, res) => res.send ('Incorrect EndPoint'));
-
 
 app.post('/ReadProvider', function(req, res) {
     let id = req.body.id;
@@ -131,8 +133,19 @@ app.post('/DeleteProvider', function(req, res) {
 });
 
 
+app.post('/RunProgram', function(req, res) {
 
-
+    let {id,nombre,direccion,telefono,fax} = req.body;
+    
+    let Line = id + '\t' + nombre + '\t' + direccion + '\t' + telefono + '\t' + fax;
+    exec('./appendText.sh ' + Line, (err, stdout) =>{
+        if(err){
+            throw err;
+        }
+    });
+    res.send ('Append Line!!!!');
+    res.status(204);
+});
 
 
  
